6b4113110588107644ccdda2c1aa130986665779
markjericho <jerichodon1998@gmail.com>
2023-08-07 23:48:24 +0800

    Improve the Matching Algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,6 +20,9 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    # Lists turned into sets
+    fave_numbers_1 = set(fave_numbers_1)
+    fave_numbers_2 = set(fave_numbers_2)
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
